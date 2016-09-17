30d917df4a5afd256a37e27584e4e534b70c2644
Timo Horstschäfer <mail@timo-horstschaefer.de>
2016-09-17 17:39:21 +0200

    update

--- src/app/home/eventList.jsx
+++ src/app/home/eventList.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 
 import Event from './event.jsx';
 
+import ReactFireMixin from 'reactfire';
+import firebase from 'firebase';
+
 import {List} from 'material-ui';
 
-class EventList extends React.Component {
-  render() {
-    var eventNodes = this.props.events.map(function(event) {
+var EventList = React.createClass({
+  mixins: [ReactFireMixin],
+  getInitialState: function() {
+    return {
+      events: [],
+      tags: []
+    };
+  },
+  componentWillMount: function() {
+    this.bindAsArray(firebase.database().ref('tags'), 'tags');
+    this.bindAsArray(firebase.database().ref('events'), 'events');
+  },
+  render: function() {
+    var eventNodes = this.state.events.map(function(event) {
         return (
-          <Event key={event.$id} tags={this.props.tags} {...event} />
+          <Event key={event.$id} {...event} tag={this.state.tags[event.tag].en} />
         );
       }, this);
     return (
@@ -17,6 +31,6 @@ class EventList extends React.Component {
       </List>
     );
   }
-}
+});
 
 export default EventList;
