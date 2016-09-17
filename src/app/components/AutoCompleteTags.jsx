e518ff0440a4c763605aa927445d2d3c2d9a107e
Kretz Mathis <mathis.kretz@swisscom.com>
2016-09-17 22:14:01 +0200

    refactored to class pattern, implemented proper dropdown values

--- src/app/components/AutoCompleteTags.jsx
+++ src/app/components/AutoCompleteTags.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
 import AutoComplete from 'material-ui/AutoComplete';
+import ReactFireMixin from 'reactfire';
+import firebase from 'firebase';
+import _ from 'lodash';
 
-class AutoCompleteTags extends React.Component {
+const AutoCompleteTags = React.createClass({
+  mixins: [ReactFireMixin],
+
+  getInitialState: function() {
+    return {
+      profile: undefined,
+      tags: []
+    };
+  },
+
+  componentWillMount: function() {
+    this.bindAsArray(firebase.database().ref('tags'), 'tags');
+    this.bindAsObject(firebase.database().ref('users/0'), 'profile');
+  },
+
+  mapAutomcompleteOptions: function () {
+    return _.map(this.state.tags,(tag) => tag.en);
+  },
+
+  alreadyUsed: function (tag) {
+    return _.find(this.getProfileTags(),(profileTag) => profileTag === this.state.tags.indexOf(tag));
+  },
+
+  removeAlreadyUsedOptions: function() {
+    return _.filter(this.state.tags,(tag) => !this.getProfileTags().length === 0 && !this.alreadyUsed(tag));
+  },
+
+  getProfileTags: function () {
+    return (this.state.profile && this.state.profile.tags ? this.state.profile.tags : [])
+  },
+
+  render: function () {
+    return (
+        <div>
+          <AutoComplete
+              hintText="Tag ..."
+              dataSource={this.mapAutomcompleteOptions()}
+          />
+        </div>
+    );
+  }
+
+});
+
+class AutoCompleteTagsi extends React.Component {
 
   constructor(props) {
     super(props);
